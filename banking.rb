beef85d1a8c4a5bd5a6908502679e90d3698b87d
Nina Leung <nleung320@gmail.com>
2015-11-02 21:08:44 -0600

    10 digit account numbers

--- banking.rb
+++ banking.rb
@@ -7,7 +7,7 @@ acct_info = []
   l_name = gets.chomp
   puts "Enter e-mail:"
   email = gets.chomp
-  person = { first_name: f_name, last_name: l_name, e_addy: email }
+  person = { first_name: f_name, last_name: l_name, e_addy: email, acct_num: rand(1000000000..9999999999) }
   acct_info << person
 end
 
